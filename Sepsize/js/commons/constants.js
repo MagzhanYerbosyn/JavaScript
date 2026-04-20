f366697ddc219015ab9efb95e984f3b924a5092c
Yerbossyn <erbosynmagzan@gmail.com>
2026-04-20 13:53:14 +0500

    Добавил функционал при сабмитте. Когда все поля введены и происходит сабмит, то последний столбец yi*Mi заполняется новыми значениями, полученными в результате перемножения mole fraction and molecular weight

--- Sepsize/js/commons/constants.js
+++ Sepsize/js/commons/constants.js
@@ -1,1 +1,3 @@
-const formElement = document.querySelector('[data-js-operating-inputs]');
+const operatingInputsFormElement = document.querySelector('[data-js-operating-inputs]');
+
+const flashInputsFormElement = document.querySelector('[data-js-flash-inputs]');
--- Sepsize/js/index.js
+++ Sepsize/js/index.js
@@ -1,8 +1,27 @@
-let formData = new FormData();
+let operatingInputs = new FormData();
+let flashInputs = new FormData();
 
-formElement.addEventListener('submit', (event) => {
+operatingInputsFormElement.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  formData = new FormData(formElement);
-  console.log(Object.fromEntries(formData));
+  operatingInputs = new FormData(operatingInputsFormElement);
+  console.log('Operating Inputs: ', Object.fromEntries(operatingInputs));
+});
+
+const molecularWeight = document.querySelectorAll('[data-js-molecularWeight]');
+console.log(molecularWeight);
+const product = document.querySelectorAll('[data-js-yi-Mi]');
+
+flashInputsFormElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  flashInputs = new FormData(flashInputsFormElement);
+  console.log('Flash Inputs: ', Object.fromEntries(flashInputs));
+  for (let i = 0; i < molecularWeight.length; i++) {
+    product[i].textContent = (
+      (parseFloat(molecularWeight[i].textContent) * Array.from(flashInputs)[i][1]) /
+      100
+    ).toFixed(3);
+    console.log(Array.from(flashInputs)[i][1]);
+  }
 });
